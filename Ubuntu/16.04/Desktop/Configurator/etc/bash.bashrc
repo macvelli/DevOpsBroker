c4f79a3d16f0512e70914d5da174462fa2ffb271
Edward Smith <edwardsmith@devopsbroker.org>
2018-08-30 06:12:32 -0500

    AppArmor tweaks for evince; Configs for sudo; System configuration cleanup

--- Ubuntu/16.04/Desktop/Configurator/etc/bash.bashrc
+++ Ubuntu/16.04/Desktop/Configurator/etc/bash.bashrc
@@ -47,12 +47,12 @@ shopt -s checkwinsize
 
 # Set variable identifying the chroot you work in (used in the prompt below)
 if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
-  debian_chroot=$(cat /etc/debian_chroot)
+	debian_chroot=$(cat /etc/debian_chroot)
 fi
 
 # Set a fancy prompt (colored if Terminal supports it)
 case "$TERM" in
-  xterm-color|*-256color) color_prompt=yes;;
+	xterm-color|*-256color) color_prompt=yes;;
 esac
 
 # Uncomment for a colored prompt, if the terminal has the capability; turned
@@ -61,20 +61,20 @@ esac
 #force_color_prompt=yes
 
 if [ -n "$force_color_prompt" ]; then
-  if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-    # We have color support; assume it's compliant with Ecma-48
-    # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
-    # a case would tend to support setf rather than setaf.)
-    color_prompt=yes
-  else
-    color_prompt=
-  fi
+	if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+		# We have color support; assume it's compliant with Ecma-48 (ISO/IEC-6429).
+		# (Lack of such support is extremely rare, and such a case would tend
+		# to support setf rather than setaf.)
+		color_prompt=yes
+	else
+		color_prompt=
+	fi
 fi
 
 if [ "$color_prompt" = yes ]; then
-  PS1='${debian_chroot:+($debian_chroot)}\[[1;38;2;124;231;124m\]\u@\h\[[0m\]:\[[1;38;2;170;190;235m\]\w\[[0m\]\$ '
+	PS1='${debian_chroot:+($debian_chroot)}\[[1;38;2;124;231;124m\]\u@\h\[[0m\]:\[[1;38;2;170;190;235m\]\w\[[0m\]\$ '
 else
-  PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+	PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
 fi
 unset color_prompt force_color_prompt
 
@@ -90,53 +90,53 @@ unset color_prompt force_color_prompt
 
 # Enable Bash completion in interactive shells
 if ! shopt -oq posix; then
-  if [ -f /usr/share/bash-completion/bash_completion ]; then
-    . /usr/share/bash-completion/bash_completion
-  elif [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
-  fi
+	if [ -f /usr/share/bash-completion/bash_completion ]; then
+		. /usr/share/bash-completion/bash_completion
+	elif [ -f /etc/bash_completion ]; then
+		. /etc/bash_completion
+	fi
 fi
 
 # sudo hint
 if [ ! -e "$HOME/.sudo_as_admin_successful" ] && [ ! -e "$HOME/.hushlogin" ] ; then
-  case " $(groups) " in *\ admin\ *|*\ sudo\ *)
-    if [ -x /usr/bin/sudo ]; then
-      echo 'To run a command as administrator (user "root"), use "sudo <command>".'
-      echo 'See "man sudo_root" for details.'
-    fi
-  esac
+	case " $(groups) " in *\ admin\ *|*\ sudo\ *)
+		if [ -x /usr/bin/sudo ]; then
+			echo 'To run a command as administrator (user "root"), use "sudo <command>".'
+			echo 'See "man sudo_root" for details.'
+		fi
+	esac
 fi
 
 # if the command-not-found package is installed, use it
 if [ -x /usr/lib/command-not-found -o -x /usr/share/command-not-found/command-not-found ]; then
-  function command_not_found_handle {
-    # check because c-n-f could've been removed in the meantime
-    if [ -x /usr/lib/command-not-found ]; then
-      /usr/lib/command-not-found -- "$1"
-      return $?
-    elif [ -x /usr/share/command-not-found/command-not-found ]; then
-      /usr/share/command-not-found/command-not-found -- "$1"
-      return $?
-    else
-      printf "%s: command not found\n" "$1" >&2
-      return 127
-    fi
-  }
+	function command_not_found_handle {
+		# check because c-n-f could've been removed in the meantime
+		if [ -x /usr/lib/command-not-found ]; then
+			/usr/lib/command-not-found -- "$1"
+			return $?
+		elif [ -x /usr/share/command-not-found/command-not-found ]; then
+			/usr/share/command-not-found/command-not-found -- "$1"
+			return $?
+		else
+			printf "%s: command not found\n" "$1" >&2
+			return 127
+		fi
+	}
 fi
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~ DevOpsBroker Configurations ~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Source /etc/devops/ansi.conf
 if [ -f /etc/devops/ansi.conf ]; then
-  . /etc/devops/ansi.conf
+	source /etc/devops/ansi.conf
 fi
 
 # Source /etc/devops/exec.conf
 if [ -f /etc/devops/exec.conf ]; then
-  . /etc/devops/exec.conf
+  source /etc/devops/exec.conf
 fi
 
 # Source /etc/devops/functions.conf
 if [ -f /etc/devops/functions.conf ]; then
-  . /etc/devops/functions.conf
+	source /etc/devops/functions.conf
 fi
--- Ubuntu/16.04/Desktop/Configurator/etc/configure-system.sh
+++ Ubuntu/16.04/Desktop/Configurator/etc/configure-system.sh
@@ -28,6 +28,7 @@
 # o /etc/modules
 # o /etc/ntp.conf
 # o /etc/profile
+# o /etc/apparmor.d/local/usr.bin.evince
 # o /etc/devops/ansi.conf
 # o /etc/devops/exec.conf
 # o /etc/devops/functions.conf
@@ -39,14 +40,16 @@
 # o /etc/skel/.gitconfig
 # o /etc/skel/.profile
 # o /etc/skel/.config/gtk-3.0/gtk.css
+# o /etc/sudoers.d/10-umask
+# o /etc/sudoers.d/20-env_keep
 #
 # Also creates the following directories for users of the system to utilize:
 # o /cache
 #
 # Other configuration tasks include:
-# o Enable the PAM_UMASK module and set the global UMASK
 # o Fix Default Applications for Common MIME Types
 # o Show Hidden Startup Applications
+# o Disable root login
 #
 # Useful Linux Command-Line Utilities
 # ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
@@ -59,21 +62,21 @@
 
 # Load /etc/devops/ansi.conf if ANSI_CONFIG is unset
 if [ -z "$ANSI_CONFIG" ] && [ -f /etc/devops/ansi.conf ]; then
-  source /etc/devops/ansi.conf
+	source /etc/devops/ansi.conf
 fi
 
 ${ANSI_CONFIG?"[1;38;2;255;100;100mCannot load '/etc/devops/ansi.conf': No such file[0m"}
 
 # Load /etc/devops/exec.conf if EXEC_CONFIG is unset
 if [ -z "$EXEC_CONFIG" ] && [ -f /etc/devops/exec.conf ]; then
-  source /etc/devops/exec.conf
+	source /etc/devops/exec.conf
 fi
 
 ${EXEC_CONFIG?"${bold}${bittersweet}Cannot load '/etc/devops/exec.conf': No such file${reset}"}
 
 # Load /etc/devops/functions.conf if FUNC_CONFIG is unset
 if [ -z "$FUNC_CONFIG" ] && [ -f /etc/devops/functions.conf ]; then
-  source /etc/devops/functions.conf
+	source /etc/devops/functions.conf
 fi
 
 ${FUNC_CONFIG?"${bold}${bittersweet}Cannot load '/etc/devops/functions.conf': No such file${reset}"}
@@ -84,58 +87,60 @@ SCRIPT_DIR="${SCRIPT_INFO[0]}"
 SCRIPT_EXEC="${SCRIPT_INFO[1]}"
 
 # Display error if not running as root
-if [ "$EUID" -ne 0 ]; then
-  echo "${bold}$SCRIPT_EXEC: ${bittersweet}Permission denied (you must be root)${reset}"
-
-  exit 1
+if [ "$USER" != 'root' ]; then
+	printError "$SCRIPT_EXEC" 'Permission denied (you must be root)'
+	exit 1
 fi
 
 ################################## Functions ##################################
 
 # ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
-# Function:	installSkeleton
-# Description:	Installs the DevOpsBroker settings file in the /etc/skel
-#		directory
+# Function:     installSkeleton
+# Description:  Installs the DevOpsBroker settings file in the /etc/skel directory
 #
-# Parameter $1:	The name of the DevOpsBroker settings file to install
-# Parameter $2:	The name of the /etc/skel file
+# Parameter $1: The name of the DevOpsBroker settings file to install
+# Parameter $2: The name of the /etc/skel file
 # Parameter $3: The directory where to install the file
 # -----------------------------------------------------------------------------
 function installSkeleton() {
+	if [ "$SCRIPT_DIR/../home/$1" -nt $3/$2 ]; then
+		printInfo "Installing $3/$2"
 
-  if [ "$SCRIPT_DIR/../home/$1" -nt $3/$2 ]; then
-    printInfo "Installing $3/$2"
+		# Install as root:root with rw-r--r-- privileges
+		$EXEC_INSTALL -o root -g root -m 644 "$SCRIPT_DIR/../home/$1" $3/$2
 
-    # Install as root:root with rw-r--r-- privileges
-    $EXEC_INSTALL -o root -g root -m 644 "$SCRIPT_DIR/../home/$1" $3/$2
-
-    echoOnExit=true
-  fi
-
+		echoOnExit=true
+	fi
 }
 
 ################################## Variables ##################################
 
+## Bash exec variables
+EXEC_LSCPU=/usr/bin/lscpu
+EXEC_LSPCI=/usr/bin/lspci
+EXEC_PASSWD=/usr/bin/passwd
+
+## Variables
 echoOnExit=false
 
 ################################### Actions ###################################
 
 # Clear screen only if called from command line
 if [ $SHLVL -eq 1 ]; then
-  clear
+	clear
 fi
 
 bannerMsg='DevOpsBroker Ubuntu 16.04 Desktop System Configurator'
 
 echo ${bold} ${wisteria}
 echo '╔═══════════════════════════════════════════════════════╗'
-echo "║ ${white}$bannerMsg${wisteria}"			     '║'
+echo "║ ${white}$bannerMsg${wisteria}"                       '║'
 echo '╚═══════════════════════════════════════════════════════╝'
 echo ${reset}
 
 # Create /etc/skel/.config/gtk-3.0 directory
 if [ ! -d /etc/skel/.config/gtk-3.0 ]; then
-  $EXEC_MKDIR --mode=0700 /etc/skel/.config/gtk-3.0
+	$EXEC_MKDIR --mode=0700 /etc/skel/.config/gtk-3.0
 fi
 
 # Install /etc/adduser.conf
@@ -153,6 +158,9 @@ installConfig 'ntp.conf' "$SCRIPT_DIR" /etc 'ntp'
 # Install /etc/profile
 installConfig 'profile' "$SCRIPT_DIR" /etc
 
+# Install /etc/apparmor.d/local/usr.bin.evince
+installConfig 'usr.bin.evince' "$SCRIPT_DIR"/apparmor.d/local /etc/apparmor.d/local
+
 # Install /etc/devops/ansi.conf
 installConfig 'ansi.conf' "$SCRIPT_DIR"/devops /etc/devops
 
@@ -162,8 +170,10 @@ installConfig 'exec.conf' "$SCRIPT_DIR"/devops /etc/devops
 # Install /etc/devops/functions.conf
 installConfig 'functions.conf' "$SCRIPT_DIR"/devops /etc/devops
 
-# Install /etc/modprobe.d/kvm-amd.conf
-installConfig 'kvm-amd.conf' "$SCRIPT_DIR"/modprobe.d /etc/modprobe.d
+if $EXEC_LSCPU | $EXEC_GREP -q 'AMD'; then
+	# Install /etc/modprobe.d/kvm-amd.conf
+	installConfig 'kvm-amd.conf' "$SCRIPT_DIR"/modprobe.d /etc/modprobe.d
+fi
 
 # Install /etc/skel/.bash_aliases
 installSkeleton 'bash_aliases' '.bash_aliases' '/etc/skel'
@@ -183,62 +193,101 @@ installSkeleton 'gitconfig' '.gitconfig' '/etc/skel'
 # Install /etc/skel/.profile
 installSkeleton 'profile' '.profile' '/etc/skel'
 
-# Install /etc/skel/.config/gtk-3.0/gtk.css
-installSkeleton 'gtk.css' 'gtk.css' '/etc/skel/.config/gtk-3.0'
+# Install /etc/sudoers.d/10-umask
+if [ ! -f /etc/sudoers.d/10-umask ]; then
+	printInfo 'Installing /etc/sudoers.d/10-umask'
+
+	# Install as root:root with r--r----- privileges
+	$EXEC_INSTALL -o root -g root -m 440 "$SCRIPT_DIR"/sudoers.d/10-umask /etc/sudoers.d
+
+	echoOnExit=true
+fi
+
+# Install /etc/sudoers.d/20-env_keep
+if [ ! -f /etc/sudoers.d/20-env_keep ]; then
+	printInfo 'Installing /etc/sudoers.d/20-env_keep'
+
+	# Install as root:root with r--r----- privileges
+	$EXEC_INSTALL -o root -g root -m 440 "$SCRIPT_DIR"/sudoers.d/20-env_keep /etc/sudoers.d
+
+	echoOnExit=true
+fi
+
+#
+# X11 Configuration
+#
+
+if $EXEC_LSPCI | $EXEC_GREP -q 'NVIDIA' && [ ! -f /etc/modprobe.d/nvidia.conf ]; then
+	echo
+	echo "${bold}${yellow}NOTE: ${white}Consider optimizing your NVidia graphics card using the DevOpsBroker /etc/modprobe.d/nvidia.conf file as a guide${reset}"
+	echoOnExit=true
+fi
+
+if [ ! -f /etc/X11/xorg.conf ]; then
+	echo
+	echo "${bold}${yellow}NOTE: ${white}Consider optimizing your X11 configuration using the DevOpsBroker /etc/X11/xorg.conf file as a guide${reset}"
+	echoOnExit=true
+fi
 
 #
 # UMASK Configuration
 #
 
-if ! $EXEC_GREP -q '^session optional[[:blank:]]*pam_umask\.so' /etc/pam.d/common-session; then
-  printInfo 'Enabling pam_umask module'
+umaskModule=$($EXEC_GREP 'pam_umask.so' /etc/pam.d/common-session)
+if [ -z "$umaskModule" ] ; then
+	printInfo 'Enabling pam_umask module'
 
-echo '
-session optional			pam_umask.so' >> /etc/pam.d/common-session
+	echo -e '\nsession optional\t\t\tpam_umask.so umask=022' >> /etc/pam.d/common-session
 
-  echoOnExit=true
+	echoOnExit=true
+elif [[ "$umaskModule" != *umask=022 ]]; then
+	printInfo "Setting global umask to '022' on pam_umask.so module"
+
+	$EXEC_SED --in-place -e 's/.+pam_umask.so$/& umask=022/' /etc/pam.d/common-session
+
+	echoOnExit=true
 fi
 
-if [ ! -f /etc/login.defs.orig ]; then
-  printInfo 'Configuring global umask'
-
-  # Backup original /etc/login.defs file
-  $EXEC_CP /etc/login.defs /etc/login.defs.orig
-
-  # Modify /etc/login.defs to configure global umask
-  $EXEC_SED -i -e 's/^(UMASK[[:blank:]]*)[0-7]{3}/\1027/' -e 's/^(USERGROUPS_ENAB[[:blank:]]*)yes/\1no/' /etc/login.defs
-
-  echoOnExit=true
-fi
+#if [ ! -f /etc/login.defs.orig ]; then
+#	printInfo 'Configuring global umask'
+#
+#	# Backup original /etc/login.defs file
+#	$EXEC_CP /etc/login.defs /etc/login.defs.orig
+#
+#	# Modify /etc/login.defs to configure global umask
+#	$EXEC_SED -i -e 's/^(UMASK[[:blank:]]*)[0-7]{3}/\1027/' -e 's/^(USERGROUPS_ENAB[[:blank:]]*)yes/\1no/' /etc/login.defs
+#
+#	echoOnExit=true
+#fi
 
 #
 # Fix Default Applications for Common MIME Types
 #
 
 if (( $($EXEC_GREP -F 'audio/mp4=rhythmbox.desktop' /usr/share/applications/defaults.list /etc/gnome/defaults.list | $EXEC_WC -l) < 2 )); then
-  # BEGIN Fix Default Applications
+	# BEGIN Fix Default Applications
 
-  printBanner 'Fixing Default Applications for Common MIME Types'
+	printBanner 'Fixing Default Applications for Common MIME Types'
 
-  printInfo 'Change default application for common audio files to Rhythmbox'
+	printInfo 'Change default application for common audio files to Rhythmbox'
 
-  # Fix /usr/share/applications/defaults.list audio defaults
-  $EXEC_SED -i 's/^(audio\/)(ac3|mp4|mpeg|x-m4a|x-mp3|x-mpeg|x-ms-wma|x-pn-aiff|x-pn-wav|x-wav)=.*/\1\2=rhythmbox.desktop/' /usr/share/applications/defaults.list
+	# Fix /usr/share/applications/defaults.list audio defaults
+	$EXEC_SED -i 's/^(audio\/)(ac3|mp4|mpeg|x-m4a|x-mp3|x-mpeg|x-ms-wma|x-pn-aiff|x-pn-wav|x-wav)=.*/\1\2=rhythmbox.desktop/' /usr/share/applications/defaults.list
 
-  # Fix /etc/gnome/defaults.list audio defaults
-  $EXEC_SED -i 's/^(audio\/)(ac3|mp4|mpeg|x-m4a|x-mp3|x-mpeg|x-ms-wma|x-pn-aiff|x-pn-wav|x-wav)=.*/\1\2=rhythmbox.desktop/' /etc/gnome/defaults.list
+	# Fix /etc/gnome/defaults.list audio defaults
+	$EXEC_SED -i 's/^(audio\/)(ac3|mp4|mpeg|x-m4a|x-mp3|x-mpeg|x-ms-wma|x-pn-aiff|x-pn-wav|x-wav)=.*/\1\2=rhythmbox.desktop/' /etc/gnome/defaults.list
 
-  printInfo 'Change default application for video files to VLC'
+	printInfo 'Change default application for video files to VLC'
 
-  # Fix /usr/share/applications/defaults.list video defaults
-  $EXEC_SED -i 's/^(.*video.*=).+/\1vlc_vlc.desktop/' /usr/share/applications/defaults.list
+	# Fix /usr/share/applications/defaults.list video defaults
+	$EXEC_SED -i 's/^(.*video.*=).+/\1vlc_vlc.desktop/' /usr/share/applications/defaults.list
 
-  # Fix /etc/gnome/defaults.list video defaults
-  $EXEC_SED -i 's/^(.*video.*=).+/\1vlc_vlc.desktop/' /etc/gnome/defaults.list
+	# Fix /etc/gnome/defaults.list video defaults
+	$EXEC_SED -i 's/^(.*video.*=).+/\1vlc_vlc.desktop/' /etc/gnome/defaults.list
 
-  echoOnExit=true
+	echoOnExit=true
 
-  # END Fix Default Applications
+	# END Fix Default Applications
 fi
 
 #
@@ -246,15 +295,26 @@ fi
 #   o Ubuntu Search -> Startup Applications
 #
 if $EXEC_GREP -Fq 'NoDisplay=true' /etc/xdg/autostart/*.desktop; then
-  printInfo 'Show hidden startup applications under Ubuntu Search -> Startup Applications'
+	printInfo 'Show hidden startup applications under Ubuntu Search -> Startup Applications'
 
-  $EXEC_SED -i 's/NoDisplay=true/NoDisplay=false/g' /etc/xdg/autostart/*.desktop
+	$EXEC_SED -i 's/NoDisplay=true/NoDisplay=false/g' /etc/xdg/autostart/*.desktop
 
-  echoOnExit=true
+	echoOnExit=true
+fi
+
+#
+# Disable root login
+#
+if [[ $($EXEC_PASSWD -S root) != "root L"* ]]; then
+	printInfo 'Disabling root login'
+
+	$EXEC_PASSWD -l root
+
+	echoOnExit=true
 fi
 
 if [ "$echoOnExit" == 'true' ]; then
-  echo
+	echo
 fi
 
 exit 0
