f672455904e261d137e336b78213b27faee4bc57
Edward Smith <edwardsmith@devopsbroker.org>
2018-09-01 23:58:06 -0500

    Added udev and initcrwnd utilities

--- Ubuntu/16.04/Desktop/Configurator/update-utils.sh
+++ Ubuntu/16.04/Desktop/Configurator/update-utils.sh
@@ -29,24 +29,24 @@
 
 # Load /etc/devops/ansi.conf if ANSI_CONFIG is unset
 if [ -z "$ANSI_CONFIG" ] && [ -f /etc/devops/ansi.conf ]; then
-  source /etc/devops/ansi.conf
+	source /etc/devops/ansi.conf
 fi
 
-${ANSI_CONFIG?"[1;38;2;255;100;100mCannot load '/etc/devops/ansi.conf': No such file[0m"}
+${ANSI_CONFIG?"[1;91mCannot load '/etc/devops/ansi.conf': No such file[0m"}
 
 # Load /etc/devops/exec.conf if EXEC_CONFIG is unset
 if [ -z "$EXEC_CONFIG" ] && [ -f /etc/devops/exec.conf ]; then
-  source /etc/devops/exec.conf
+	source /etc/devops/exec.conf
 fi
 
-${EXEC_CONFIG?"${bold}${bittersweet}Cannot load '/etc/devops/exec.conf': No such file${reset}"}
+${EXEC_CONFIG?"[1;91mCannot load '/etc/devops/exec.conf': No such file[0m"}
 
 # Load /etc/devops/functions.conf if FUNC_CONFIG is unset
 if [ -z "$FUNC_CONFIG" ] && [ -f /etc/devops/functions.conf ]; then
-  source /etc/devops/functions.conf
+	source /etc/devops/functions.conf
 fi
 
-${FUNC_CONFIG?"${bold}${bittersweet}Cannot load '/etc/devops/functions.conf': No such file${reset}"}
+${FUNC_CONFIG?"[1;91mCannot load '/etc/devops/functions.conf': No such file[0m"}
 
 ## Script information
 SCRIPT_INFO=( $($EXEC_SCRIPTINFO "$BASH_SOURCE") )
@@ -54,127 +54,110 @@ SCRIPT_DIR="${SCRIPT_INFO[0]}"
 SCRIPT_EXEC="${SCRIPT_INFO[1]}"
 
 # Display error if not running as root
-if [ "$EUID" -ne 0 ]; then
-  echo "${bold}$SCRIPT_EXEC: ${bittersweet}Permission denied (you must be root)${reset}"
-
-  exit 1
+if [ "$USER" != 'root' ]; then
+	printError "$SCRIPT_EXEC" 'Permission denied (you must be root)'
+	exit 1
 fi
 
 ################################## Functions ##################################
 
 # ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
-# Function:	createSymlink
-# Description:	Creates a symbolic link to the specified file
+# Function:     createSymlink
+# Description:  Creates a symbolic link to the specified file
 #
-# Parameter $1:	The name of the symbolic link to create
-# Parameter $2:	The target file to link to
+# Parameter $1: The name of the symbolic link to create
+# Parameter $2: The target file to link to
 # -----------------------------------------------------------------------------
 function createSymlink() {
-  # BEGIN create symbolic link
+	local symlink="$1"
+	local targetFile="$2"
 
-  local symlink="$1"
-  local targetFile="$2"
-
-  if [ ! -L "$symlink" ]; then
-    printInfo "Creating symbolic link $symlink"
-    $EXEC_LN -s "$targetFile" "$symlink"
-    $EXEC_CHOWN --no-dereference root:users "$symlink"
-    echoOnExit=true
-  fi
-
-  # END create symbolic link
+	if [ ! -L "$symlink" ]; then
+		printInfo "Creating symbolic link $symlink"
+		$EXEC_LN -s "$targetFile" "$symlink"
+		$EXEC_CHOWN --no-dereference root:users "$symlink"
+		echoOnExit=true
+	fi
 }
 
 # ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
-# Function:	installSystemUtility
-# Description:	Installs the system administration utility into /usr/local/sbin
+# Function:     installSystemUtility
+# Description:  Installs the system administration utility into /usr/local/sbin
 #
-# Parameter $1:	The name of the utility to install
-# Parameter $2:	The source directory (where to find the utility to install)
+# Parameter $1: The name of the utility to install
+# Parameter $2: The source directory (where to find the utility to install)
 # -----------------------------------------------------------------------------
 function installSystemUtility() {
-  # BEGIN install system utility function
+	local utility="$1"
+	local sourceDir="$2"
 
-  local utility="$1"
-  local sourceDir="$2"
+	if [ ! -f /usr/local/sbin/$utility ]; then
+		printInfo "Installing system utility /usr/local/sbin/$utility"
 
-  if [ ! -f /usr/local/sbin/$utility ]; then
-    printInfo "Installing system utility /usr/local/sbin/$utility"
+		# Install system utility as root:sudo with rwxr-x--- privileges
+		$EXEC_INSTALL -o root -g sudo -m 750 "$sourceDir/$utility" /usr/local/sbin
+		echoOnExit=true
 
-    # Install system utility as root:sudo with rwxr-x--- privileges
-    $EXEC_INSTALL -o root -g sudo -m 750 "$sourceDir/$utility" /usr/local/sbin
-    echoOnExit=true
+	elif [ "$sourceDir/$utility" -nt /usr/local/sbin/$utility ]; then
+		printInfo "Updating system utility /usr/local/sbin/$utility"
 
-  elif [ "$sourceDir/$utility" -nt /usr/local/sbin/$utility ]; then
-    printInfo "Updating system utility /usr/local/sbin/$utility"
-
-    # Install system utility as root:sudo with rwxr-x--- privileges
-    $EXEC_INSTALL -o root -g sudo -m 750 "$sourceDir/$utility" /usr/local/sbin
-    echoOnExit=true
-  fi
-
-  # END install system utility function
+		# Install system utility as root:sudo with rwxr-x--- privileges
+		$EXEC_INSTALL -o root -g sudo -m 750 "$sourceDir/$utility" /usr/local/sbin
+		echoOnExit=true
+	fi
 }
 
 # ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
-# Function:	installUserUtility
-# Description:	Installs the user utility into /usr/local/bin
+# Function:     installUserUtility
+# Description:  Installs the user utility into /usr/local/bin
 #
-# Parameter $1:	The name of the utility to install
-# Parameter $2:	The source directory (where to find the utility to install)
+# Parameter $1: The name of the utility to install
+# Parameter $2: The source directory (where to find the utility to install)
 # -----------------------------------------------------------------------------
 function installUserUtility() {
-  # BEGIN install user utility function
+	local utility="$1"
+	local sourceDir="$2"
 
-  local utility="$1"
-  local sourceDir="$2"
+	if [ ! -f /usr/local/bin/$utility ]; then
+		printInfo "Installing user utility /usr/local/bin/$utility"
 
-  if [ ! -f /usr/local/bin/$utility ]; then
-    printInfo "Installing user utility /usr/local/bin/$utility"
+		# Install user utility as root:users with rwxr-xr-x privileges
+		$EXEC_INSTALL -o root -g users -m 755 "$sourceDir/$utility" /usr/local/bin
+		echoOnExit=true
 
-    # Install user utility as root:users with rwxr-xr-x privileges
-    $EXEC_INSTALL -o root -g users -m 755 "$sourceDir/$utility" /usr/local/bin
-    echoOnExit=true
+	elif [ "$sourceDir/$utility" -nt /usr/local/bin/$utility ]; then
+		printInfo "Updating user utility /usr/local/bin/$utility"
 
-  elif [ "$sourceDir/$utility" -nt /usr/local/bin/$utility ]; then
-    printInfo "Updating user utility /usr/local/bin/$utility"
-
-    # Install user utility as root:users with rwxr-xr-x privileges
-    $EXEC_INSTALL -o root -g users -m 755 "$sourceDir/$utility" /usr/local/bin
-    echoOnExit=true
-  fi
-
-  # END install user utility function
+		# Install user utility as root:users with rwxr-xr-x privileges
+		$EXEC_INSTALL -o root -g users -m 755 "$sourceDir/$utility" /usr/local/bin
+		echoOnExit=true
+	fi
 }
 
 # ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
-# Function:	installTemplate
-# Description:	Installs the venture template to /usr/local/bin/template
+# Function:     installTemplate
+# Description:  Installs the venture template to /usr/local/bin/template
 #
-# Parameter $1:	The name of the template to install
+# Parameter $1: The name of the template to install
 # -----------------------------------------------------------------------------
 function installTemplate() {
-  # BEGIN install venture template function
+	local template="$1"
+	local sourceDir="$SCRIPT_DIR"/usr/local/bin/template
 
-  local template="$1"
-  local sourceDir="$SCRIPT_DIR"/usr/local/bin/template
+	if [ ! -f /usr/local/bin/template/$template ]; then
+		printInfo "Installing venture template /usr/local/bin/template/$template"
 
-  if [ ! -f /usr/local/bin/template/$template ]; then
-    printInfo "Installing venture template /usr/local/bin/template/$template"
+		# Install venture template as root:users with rwxr-xr-x privileges
+		$EXEC_INSTALL -o root -g users -m 755 "$sourceDir/$template" /usr/local/bin/template
+		echoOnExit=true
 
-    # Install venture template as root:users with rwxr-xr-x privileges
-    $EXEC_INSTALL -o root -g users -m 755 "$sourceDir/$template" /usr/local/bin/template
-    echoOnExit=true
+	elif [ "$sourceDir/$template" -nt /usr/local/bin/template/$template ]; then
+		printInfo "Updating venture template /usr/local/bin/template/$template"
 
-  elif [ "$sourceDir/$template" -nt /usr/local/bin/template/$template ]; then
-    printInfo "Updating venture template /usr/local/bin/template/$template"
-
-    # Install venture template as root:users with rwxr-xr-x privileges
-    $EXEC_INSTALL -b --suffix=.bak -o root -g users -m 755 "$sourceDir/$template" /usr/local/bin/template
-    echoOnExit=true
-  fi
-
-  # END install venture template function
+		# Install venture template as root:users with rwxr-xr-x privileges
+		$EXEC_INSTALL -b --suffix=.bak -o root -g users -m 755 "$sourceDir/$template" /usr/local/bin/template
+		echoOnExit=true
+	fi
 }
 
 ################################## Variables ##################################
@@ -184,24 +167,18 @@ echoOnExit=false
 
 ################################### Actions ###################################
 
-# Clear screen and print banner only if called from command line
+# Clear screen only if called from command line
 if [ $SHLVL -eq 1 ]; then
-  clear
-
-  bannerMsg='DevOpsBroker Ubuntu 16.04 Desktop Utility Updater'
-
-  echo ${bold} ${wisteria}
-  echo '╔═══════════════════════════════════════════════════╗'
-  echo "║ ${white}$bannerMsg${wisteria}"                   '║'
-  echo '╚═══════════════════════════════════════════════════╝'
-  echo ${reset}
+	clear
 fi
+
+printBox "DevOpsBroker $UBUNTU_RELEASE Desktop Utility Updater" 'true'
 
 # Build all C language utilties
 /usr/bin/make -s --directory="$SCRIPT_DIR"/C
 
 if [ "$SCRIPT_DIR" != "$INSTALL_DIR" ]; then
-  $EXEC_CHOWN -R $SUDO_USER:$SUDO_USER "$SCRIPT_DIR"/C
+	$EXEC_CHOWN -R $SUDO_USER:$SUDO_USER "$SCRIPT_DIR"/C
 fi
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~ System Administration ~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -217,6 +194,9 @@ installSystemUtility disableJournal "$SCRIPT_DIR"/usr/local/sbin
 
 # Install firewall system administration utility
 installSystemUtility firewall "$SCRIPT_DIR"/usr/local/sbin
+
+# Install initcrwnd system administration utility
+installSystemUtility initcrwnd "$SCRIPT_DIR"/usr/local/sbin
 
 # Install pms system administration utility
 installSystemUtility pms "$SCRIPT_DIR"/usr/local/sbin
@@ -310,6 +290,9 @@ installUserUtility ssh-key "$SCRIPT_DIR"/usr/local/bin
 # Install symlink user utility
 installUserUtility symlink "$SCRIPT_DIR"/usr/local/bin
 
+# Install udev user utility
+installUserUtility udev "$SCRIPT_DIR"/usr/local/bin
+
 # Install uuid user utility
 installUserUtility uuid "$SCRIPT_DIR"/usr/local/bin
 
@@ -326,11 +309,11 @@ installUserUtility verifyip "$SCRIPT_DIR"/C
 
 # Make /usr/local/bin/template directory
 if [ ! -d /usr/local/bin/template ]; then
-  printInfo 'Creating /usr/local/bin/template directory'
+	printInfo 'Creating /usr/local/bin/template directory'
 
-  $EXEC_MKDIR --mode=755 /usr/local/bin/template
-  $EXEC_CHOWN root:users /usr/local/bin/template
-  echoOnExit=true
+	$EXEC_MKDIR --mode=755 /usr/local/bin/template
+	$EXEC_CHOWN root:users /usr/local/bin/template
+	echoOnExit=true
 fi
 
 installTemplate 'bash-script.tpl'
@@ -341,7 +324,7 @@ installTemplate 'makefile.tpl'
 installTemplate 'source-file-c.tpl'
 
 if [ $echoOnExit == 'true' ]; then
-  echo
+	echo
 fi
 
 exit 0
